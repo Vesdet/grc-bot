3fc853000506911621dbf8fec9a8b659b6e32f23
HackuunaMatata <hackuuna.matata@gmail.com>
2020-03-21 23:35:39 +0300

    Token update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const Telegraf = require('telegraf');
 
-
-const TOKEN = "1134761566:AAHVfjmpmP2f488WwvPaZrKBuA-PeWwSXPE";
-
+const TOKEN = process.env.TELEGRAM_TOKEN;
 const bot = new Telegraf(TOKEN);
 
-bot.start((ctx) => ctx.reply('Привет! Введи имя моба, чтобы узнать какими героями его бить.'));
+bot.start((ctx) => ctx.reply('Привет! Введи имя моба, чтобы узнать какими героями его бить!'));
 
 bot.hears(/hi/i, (ctx) => ctx.reply('Hey there'));
 
