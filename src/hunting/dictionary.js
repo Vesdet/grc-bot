09d4aa911008e8289ecbc185cd4216f1c452312d
HackuunaMatata <hackuuna.matata@gmail.com>
2020-03-22 01:03:33 +0300

    add dictionary and mob selection

---
+++ src/hunting/dictionary.js
@@ -0,0 +1,56 @@
+const bonAppeti = ['тощая вивиан', 'вивиан', 'вивианка', 'bon appeti'];
+
+const terrorthorn = ['шип ужаса', 'шип', 'terrorthorn'];
+
+const grimReaper = ['мрачный жнец', 'жнец', 'grim reaper'];
+
+const hardrox = ['древний голем', 'голем', 'hardrox'];
+
+const gargantua = ['гаргантюа', 'гарга', 'gargantua'];
+
+const megaMaggot = ['магот', 'mega maggot'];
+
+const gryphon = ['грифон', 'gryphon'];
+
+const tidalTitan = ['титан', 'tidal titan'];
+
+const blackwing = ['чернокрыл', 'blackwing'];
+
+const queenBee = ['королева пчёл', 'королева пчел', 'пчела', 'queen bee'];
+
+const frostwing = ['ледокрыл', 'frostwing'];
+
+const noceros = ['рино', 'noceros'];
+
+const jadeWyrm = ['нефритовый змей', 'змей', 'jade wyrm'];
+
+const saberfang = ['саблезуб', 'saberfana'];
+
+const voodooShaman = ['шаман вуду', 'вуду', 'шаман', 'voodoo shaman'];
+
+const snowBeast = ['вьюжник', 'snow beast'];
+
+const hellDrider = ['адский драйдер', 'драйдер', 'адский', 'hell drider'];
+
+const mechaTrojan = ['троянский конь', 'конь', 'троянский', 'троян', 'mecha trojan'];
+
+module.exports = {
+  bonAppeti,
+  terrorthorn,
+  grimReaper,
+  hardrox,
+  gargantua,
+  megaMaggot,
+  gryphon,
+  tidalTitan,
+  blackwing,
+  queenBee,
+  frostwing,
+  noceros,
+  jadeWyrm,
+  saberfang,
+  voodooShaman,
+  snowBeast,
+  hellDrider,
+  mechaTrojan
+};
---
+++ src/index.js
@@ -0,0 +1,26 @@
+const Telegraf = require('telegraf');
+const fs = require('fs');
+const dictionary = require('./hunting/dictionary');
+
+const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+
+const bot = new Telegraf(TOKEN);
+
+bot.start((ctx) => ctx.reply('Привет! Введи имя моба, чтобы узнать какими героями его бить!'));
+
+bot.hears(/(.*)/i, (ctx) => {
+  const mob = ctx.match[1].trim().toLowerCase();
+  const entity = Object.entries(dictionary).find(([key, value]) => value.includes(mob));
+
+  if (entity) {
+    try {
+      return ctx.replyWithPhoto({ source: fs.createReadStream(`src/hunting/images/${entity[0]}.jpg`) });
+    } catch {
+      return ctx.reply('Мне такой моб не знаком :( Попробуй ввести его другое название');
+    }
+  }
+
+  return ctx.reply('Мне такой моб не знаком :( Попробуй ввести его другое название');
+});
+
+bot.launch();
