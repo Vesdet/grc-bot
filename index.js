7f0b09c0beee4e57e914ae3ae45661ac25624ea2
Aleksandra_Ryzhova <Aleksandra_Ryzhova@epam.com>
2020-03-21 23:02:10 +0300

    Heroku deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const TOKEN = "1134761566:AAHVfjmpmP2f488WwvPaZrKBuA-PeWwSXPE";
 
 const bot = new Telegraf(TOKEN);
 
-bot.start((ctx) => ctx.reply('Welcome!'));
+bot.start((ctx) => ctx.reply('Привет! Введи имя моба, чтобы узнать какими героями его бить.'));
 
 bot.hears(/hi/i, (ctx) => ctx.reply('Hey there'));
 
