c55141e937e78139e6db6f6afbd5302463ee7bcd
HackuunaMatata <hackuuna.matata@gmail.com>
2020-03-22 01:47:42 +0300

    fix dictionary

--- src/hunting/dictionary.js
+++ src/hunting/dictionary.js
@@ -4,27 +4,27 @@ const terrorthorn = ['шип ужаса', 'шип', 'terrorthorn'];
 
 const grimReaper = ['мрачный жнец', 'жнец', 'grim reaper'];
 
-const hardrox = ['древний голем', 'голем', 'hardrox'];
+const hardrox = ['древний голем', 'камень', 'голем', 'hardrox'];
 
 const gargantua = ['гаргантюа', 'гарга', 'gargantua'];
 
 const megaMaggot = ['магот', 'mega maggot'];
 
-const gryphon = ['грифон', 'gryphon'];
+const gryphon = ['грифон', 'птичка', 'gryphon'];
 
 const tidalTitan = ['титан', 'tidal titan'];
 
 const blackwing = ['чернокрыл', 'blackwing'];
 
-const queenBee = ['королева пчёл', 'королева пчел', 'пчела', 'queen bee'];
+const queenBee = ['королева пчёл', 'королева пчел', 'пчела', 'пчелка', 'пчёлка', 'queen bee'];
 
 const frostwing = ['ледокрыл', 'frostwing'];
 
-const noceros = ['рино', 'noceros'];
+const noceros = ['рино', 'носорог', 'noceros'];
 
 const jadeWyrm = ['нефритовый змей', 'змей', 'jade wyrm'];
 
-const saberfang = ['саблезуб', 'saberfana'];
+const saberfang = ['саблезуб', 'тигр', 'saberfang'];
 
 const voodooShaman = ['шаман вуду', 'вуду', 'шаман', 'voodoo shaman'];
 
